50d45685b7908bb1b6ebcd5a02b0a1c1895f910d
BuildTools <cubemaster118@gmail.com>
2020-12-12 22:07:57 -0500

    Initial commit

---
+++ src/battleship.h
@@ -0,0 +1,21 @@
+//
+// Created by John on 12/12/2020.
+//
+
+#ifndef CS2301_HW6_BATTLESHIP_BATTLESHIP_H
+#define CS2301_HW6_BATTLESHIP_BATTLESHIP_H
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+typedef enum
+{
+    CARRIER = 'C',
+    BATTLESHIP = 'B',
+    CRUISER = 'R',
+    SUBMARINE = 'S',
+    DESTROYER = 'D'
+}symbol;
+
+
+#endif //CS2301_HW6_BATTLESHIP_BATTLESHIP_H
